7ef9baee20d0a37aa7182f359b70e84f2964d2a0
weeradon <weeradonwil1005@gmail.com>
2026-02-20 15:04:17 +0700

    update

--- backend/controllers/authControlle.js
+++ backend/controllers/authControlle.js
@@ -1,13 +1,16 @@
 //authcontroller.js ย่อมากจาก authentication controller
 //เป็นตัวจัดการเกี่ยวกับการยืนยันตัวตนของผู้ใช้ เช่น การลงทะเบีย การเข้าสู่ระบบ และการออกจากระบบ เป็นต้น
 
-
+//นำเข้าไลบรารี jsonwebtoken ซึ่งใช้สำหรับการสร้างและตรวจสอบ token คืนที่ใช้ในการยืนยันตัวตนของผู้ใช้
+//token เป็นสตริงที่ถูกสร้างขึ้นโดยเซิร์ฟเวอร์ และส่งกลับไปยังไคลเอนต์หลังจากที่ผู้ใช้เข้าสู่ระบบสำเร็จ
 import { JsonWebTokenError } from 'jsonwebtoken'
 
 //ขั้นตอนแรกคือการนำเข้าโมเดล user ซึ่งเป็นตัวแทนของผู้ใช้ในฐานข้อมูล
 import userModel from '../models/userModel.js'
+
 //นำเข้าไลบรารี bcryptjs ซึ่งใช้สำหรับการเข้ารหัสในส่วนของรหัสผ่านของผู้ใช้
 import bcrypt from 'bcryptjs'
+
 
 //ฟังก์ชันสำหรับการลงทะเบียนผู้ใช้ใหม่
 export const register = async (req, res)=>{
@@ -32,10 +35,45 @@ export const register = async (req, res)=>{
         //หากใช้ค่ามากเกินไปอาจทำให้กระบวนการช้าลง
         const hashedPassword = await bcrypt.hash(password, 10)
 
+        //สร้างอินสแตนซ์ใหม่ของ userModel โดยส่งข้อมูลที่ได้รับจากผู้ใช้และรหัสผ่านที่เข้ารหัสผ่านแล้ว
         const user = new userModel({name, email, password: hashedPassword})
-        await user.save
+        await user.save //บันทึกข้อมูลผู้ใช้ลงในฐานข้อมูล
+
+        //สร้าง token สำหรับ user ที่เพิ่งลงทะเบียนสำเร็จ และเก็บไว้ในตัวแปร token
+        //่ jwt.sign เป็นฟังก์ชันที่ใช้ในการสร้าง token โดยรับพารามิเตอร์ดังนี้
+        // 1.payload ข้อมูลที่ต้องการเก็บใน token ในที่นี้คือ id ของผู้ใช้งาน
+        // 2.secret คีย์ลับที่ใช้ในการเข้ารหัส token ซึ่งควรเก็บเป็นความลับและไม่ควรเปิดเผย
+        // 3.options: ตัวเลือกเพ่มเติม เช่น expiresIn ที่กำหนดระยะเวลาหมดอายุของ token ในที่นี้กำหนดให้ 7 วัน
+        const token = jwt.sign({id: user._id}, process.env.jwt_secret,{expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true, //ทำให้คุกกี้ไม่สามารถเข้าถึงได้จากฝั่งไคลเอนต์
+            secure: process.env.node_env === 'production', //ทำให้คุกกี้ถูกส่งผ่านเฉพาะในโปรเซคชัน
+            sameSite: process.env.node_env === 'production' ? 'none' : 'lax',
+            maxAge: 7*28*60*60*1000 //กำหนดอายุของคุกกี้เป็น 7 วัน
+        })
+
+        return res.json({
+            success: true, message: "ลงทะเบียสำเร็จแล้ว"})
+        
     }
     catch(error) {
         res.json({success: false, message: error.message})
     }
+}
+
+export const login = async = async (res, req) =>{
+
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.node_env === 'production',
+            sameSite: process.env.node_env === 'production' ? 'none' : 'lax',
+        }) 
+        return res.json({success: true, message: " ออกจากระบบ"})
+    }catch (error){
+        return res.json({success: false, message: error.message})
+    }
+
+
 }
