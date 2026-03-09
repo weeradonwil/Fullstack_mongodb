28ff4f48cb7aa634c383abe1b6e70054fe576c1f
weeradone <weeradonwil1005@gmail.com>
2026-03-09 11:00:32 +0700

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,11 +6,7 @@ const Navbar = () => {
     return (
         <div className='items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
             <button
-                onClick={() => navigate('/login')}
-                className='flex items-center gap-2 border border-gray-500
-      rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100'
-            >
-                Login
+                onClick={() => navigate('/login')}className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100'>Login
             </button>
         </div>
     )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Login() {
+const Login = () => {
+
+  const [state, setState] = useState('Sign Up')
+
+
   return (
-    <div>
-      This is Login
+    <div className='flex items-center justify-center min-h-screen px-6 bg-gradient-to-br from-blue-200 to-purple-400'>
+
+      <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full max-w-[400px] text-indigo-300 text-sm'>
+        <h2 className='text-3xl font-semibold text-white text-centor mb-3'>{state === 'Sign Up' ? 'สมัครสมาชิก' : 'เข้าสู่ระบบ'}</h2>
+
+        <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'กรุณากรอกข้อมูลเพื่อสมัครสมาชิก' : 'กรุณากรอกข้อมูลเพื่อเข้าสู่ระบบ'}</p>
+
+        <form >
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-(#333ASC)'>
+            <label className='text-gray-400'>ชื่อ-นามสกุล</label>
+            <input className='bg-transparent outline-none' type="text" placeholder="ระบุชื่อ-นามสกุล" required />
+          </div>
+
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-(#333ASC)'>
+            <label className='text-gray-400'>อีเมล</label>
+            <input className='bg-transparent outline-none' type="email" placeholder="ระบุอีเมล" required />
+          </div>
+
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-(#333ASC)'>
+            <label className='text-gray-400'>รหัสผ่าน</label>
+            <input className='bg-transparent outline-none' type="password" placeholder="ระบุรหัสผ่าน" required />
+          </div>
+
+          <p className='mb-4 text-indigo-500 curser-pointer'>ลืมรหัสผ่าน</p>
+
+          <button type='button' onClick={() => setState(state === 'Sign Up' ? 'Login' : 'Sign Up')}
+            className='mb-4 text-sm text-gray-400 hover:text-gray-200'>{state === 'Sign Up' ? 'มีบัญชีแล้ว?' : 'ยังไม่มีบัญชี? สมัครสมาชิก'}</button>
+
+          
+        </form>
+
+        <button onClick={() => setState(state === 'Sign Up' ? 'Login' : 'Sign Up')}>คลิกเพื่อ {state === 'Sign Up' ? 'Login' : 'Sign Up'}</button>
+      </div>
     </div>
   )
 }
+
 
 export default Login
